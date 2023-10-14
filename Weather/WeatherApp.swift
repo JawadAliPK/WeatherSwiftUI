0ea3724d2998a5640661fbff880b8c1081b3289d
Jawad Ali <jawad@eddress.co>
2023-10-15 01:22:36 +0200

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by jawad ali on 14/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
