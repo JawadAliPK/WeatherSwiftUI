6f8a77f91f5becea4d4eb39e740140f35d82d651
Jawad Ali <jawad@eddress.co>
2023-10-16 22:23:35 +0200

    ForecastCard added

---
+++ Weather/Views/Components/ForecastCard.swift
@@ -0,0 +1,18 @@
+//
+//  ForecastCard.swift
+//  Weather
+//
+//  Created by jawad ali on 16/10/2023.
+//
+
+import SwiftUI
+
+struct ForecastCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForecastCard()
+}
